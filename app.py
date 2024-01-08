8f61ea2b650ab0cc07e038310947d703cee13db4
Irsyadobby <98371661+Irsyadobby@users.noreply.github.com>
2024-01-08 20:09:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import json
 from ultralytics import YOLO
 import pandas as pd
+import io
 
 
 # Configurations
@@ -21,47 +22,124 @@ if CFG_ENABLE_URL_DOWNLOAD:
     url = "https://archive.org/download/yoloTrained/yoloTrained.pt"
 # End of Configurations
 
-model = YOLO('model/best.pt')
+model = YOLO('models/best.pt')
 
-def imageInput(model, src):
+# Initialize Session State
+if 'results_df' not in st.session_state:
+    st.session_state.results_df = None
+if 'image_outputs' not in st.session_state:
+    st.session_state.image_outputs = []
+if 'output_tab' not in st.session_state:
+    st.session_state.output_tab = False
+if 'input_type' not in st.session_state:
+    st.session_state.input_type = "Single"
 
+
+
+def imageInput(model, src, input_type):
     if src == 'External Data':
-        image_file = st.file_uploader(
-            "Upload An Image", type=['png', 'jpeg', 'jpg'])
-        col1, col2 = st.columns(2)
-        if image_file is not None:
-            img = Image.open(image_file)
-            with col1:
-                st.image(img, use_column_width='always')
 
-            with st.spinner(text = 'Predicting...'):
-                results = model.predict(img, hide_conf=True, line_width=1, retina_masks=True, iou=0.5, augment=True, max_det=9, agnostic_nms=True)
-                result = results[0]
-                prediction_json = result.tojson()
-                predictions = json.loads(prediction_json)
+        if st.session_state.input_type != input_type :
+            st.session_state.results_df = None
+            st.session_state.image_outputs = []
+            st.session_state.output_tab = False
+            st.session_state.input_type = input_type
+                
+        if input_type == 'Multiple' :
+            images = st.file_uploader('Select Images', type = ['jpg','png','jpeg'], accept_multiple_files = True)
 
-                with open('data/predefined_classes.txt', 'r') as f:
-                    classes = f.read().split()
+            if images:
+                st.write('Uploaded Files :')
 
-                filtered_predictions = [pred for pred in predictions if pred['name'] in classes]
-                sorted_predictions = sorted(filtered_predictions, key=lambda x: x['box']['x1'])
-                combined_classes = ''.join([pred['name'] for pred in sorted_predictions])
+                filenames = []
+                for file in images :
+                    filenames.append(file.name)
+                
+                df_image = pd.DataFrame({'Name of File' : filenames})
+                st.dataframe(df_image)
 
-                st.subheader("Model Prediction")
-                st.metric(label = "License Plate", value = combined_classes)
+            if st.button('Predict!'):
+                total_images = len(images)
+                result_list = []
 
-            with col2:
-                fig, ax = plt.subplots(figsize=(img.width / 100, img.height / 100))
-                ax.imshow(img)
-                for pred in sorted_predictions:
-                    box = pred['box']
-                    rect = plt.Rectangle((box['x1'], box['y1']), box['x2'] - box['x1'], box['y2'] - box['y1'],
-                                        fill=False, color='blue', linewidth=2)
-                    ax.add_patch(rect)
-                    font_size = int(img.width / 80)  # Adjust font size proportionally
-                    plt.text(box['x1'], box['y1'], pred['name'], color='black', backgroundcolor='white', fontsize=font_size)
-                plt.axis('off')
-                st.pyplot(fig, bbox_inches='tight', pad_inches=0)
+                for idx, uploaded_image in enumerate(images):
+                    image = Image.open(uploaded_image)
+
+                    result = model.predict(image, hide_conf=True, line_width=1, retina_masks=True, iou=0.5, augment=True, max_det=9, agnostic_nms=True)
+                    result = result[0]
+                    prediction_json = result.tojson()
+                    predictions = json.loads(prediction_json)
+
+                    with open('data/predefined_classes.txt', 'r') as f:
+                        classes = f.read().split()
+                    
+                    filtered_predictions = [pred for pred in predictions if pred['name'] in classes]
+                    sorted_predictions = sorted(filtered_predictions, key=lambda x: x['box']['x1'])
+                    combined_classes = ''.join([pred['name'] for pred in sorted_predictions])
+
+                    filename = uploaded_image.name
+                    result_list.append({'Name of File' : filename, 'Prediction' : combined_classes})
+
+                    st.session_state.image_outputs.append((uploaded_image, sorted_predictions))
+                
+                st.subheader('Model Prediction :')
+
+                st.session_state.results_df = pd.DataFrame(result_list)
+                st.dataframe(st.session_state.results_df)
+            
+            if st.session_state.output_tab is not None and st.session_state.results_df is not None :
+                csv_buffer = io.StringIO()
+                st.session_state.results_df.to_csv(csv_buffer, index = False)
+                csv_bytes = csv_buffer.getvalue()
+                st.download_button('Download Prediction (csv)', data = csv_bytes, file_name = "Model Prediction.csv", mime = 'data/csv')
+            
+
+            if st.session_state.output_tab is not None :
+                if st.button('Reset Prediction') :
+                    st.session_state.results_df = None
+                    st.session_state.image_outputs = []
+                    st.session_state.output_tab = False
+                
+                st.warning("Make sure you click 'Reset Prediction' before doing another prediction !")
+     
+        
+        if input_type == 'Single' :
+            image_file = st.file_uploader(
+                "Upload An Image", type=['png', 'jpeg', 'jpg'])
+            col1, col2 = st.columns(2)
+            if image_file is not None:
+                img = Image.open(image_file)
+                with col1:
+                    st.image(img, use_column_width='always')
+
+                with st.spinner(text = 'Predicting...'):
+                    results = model.predict(img, hide_conf=True, line_width=1, retina_masks=True, iou=0.5, augment=True, max_det=9, agnostic_nms=True)
+                    result = results[0]
+                    prediction_json = result.tojson()
+                    predictions = json.loads(prediction_json)
+
+                    with open('data/predefined_classes.txt', 'r') as f:
+                        classes = f.read().split()
+
+                    filtered_predictions = [pred for pred in predictions if pred['name'] in classes]
+                    sorted_predictions = sorted(filtered_predictions, key=lambda x: x['box']['x1'])
+                    combined_classes = ''.join([pred['name'] for pred in sorted_predictions])
+
+                    st.subheader("Model Prediction : ")
+                    st.metric(label = "License Plate", value = combined_classes)
+
+                with col2:
+                    fig, ax = plt.subplots(figsize=(img.width / 100, img.height / 100))
+                    ax.imshow(img)
+                    for pred in sorted_predictions:
+                        box = pred['box']
+                        rect = plt.Rectangle((box['x1'], box['y1']), box['x2'] - box['x1'], box['y2'] - box['y1'],
+                                            fill=False, color='blue', linewidth=2)
+                        ax.add_patch(rect)
+                        font_size = int(img.width / 80)  # Adjust font size proportionally
+                        plt.text(box['x1'], box['y1'], pred['name'], color='black', backgroundcolor='white', fontsize=font_size)
+                    plt.axis('off')
+                    st.pyplot(fig, bbox_inches='tight', pad_inches=0)
 
 
     elif src == 'Test Dataset':
@@ -82,60 +160,33 @@ def imageInput(model, src):
             st.image(img, caption='Selected Image', use_column_width='always')
         with col2:
             if image_file is not None and submit:
-                with st.spinner(text="Predicting..."):
-                    # Load model
+                with st.spinner(text = "Predicting..."):
+                    results = model.predict(img, hide_conf=True, line_width=1, retina_masks=True, iou=0.5, augment=True, max_det=9, agnostic_nms=True)
+                    result = results[0]
+                    prediction_json = result.tojson()
+                    predictions = json.loads(prediction_json)
 
-                    pred = model(image_file)
-                    pred.render()
-                    # save output to file
-                    for im in pred.ims:
-                        im_base64 = Image.fromarray(im)
-                        im_base64.save(os.path.join(
-                            'data/outputs', os.path.basename(image_file)))
-                # Display predicton
-                img_ = Image.open(os.path.join(
-                    'data/outputs', os.path.basename(image_file)))
-                st.image(img_, caption='Model Prediction(s)')
+                    with open('data/predefined_classes.txt', 'r') as f:
+                        classes = f.read().split()
 
+                    filtered_predictions = [pred for pred in predictions if pred['name'] in classes]
+                    sorted_predictions = sorted(filtered_predictions, key=lambda x: x['box']['x1'])
+                    combined_classes = ''.join([pred['name'] for pred in sorted_predictions])
 
-def videoInput(model, src):
-    if src == 'Upload your own data.':
-        uploaded_video = st.file_uploader(
-            "Upload A Video", type=['mp4', 'mpeg', 'mov'])
-        pred_view = st.empty()
-        warning = st.empty()
-        if uploaded_video != None:
-
-            # Save video to disk
-            ts = datetime.timestamp(datetime.now())  # timestamp a upload
-            uploaded_video_path = os.path.join(
-                'data/uploads', str(ts)+uploaded_video.name)
-            with open(uploaded_video_path, mode='wb') as f:
-                f.write(uploaded_video.read())
-
-            # Display uploaded video
-            with open(uploaded_video_path, 'rb') as f:
-                video_bytes = f.read()
-            st.video(video_bytes)
-            st.write("Uploaded Video")
-            submit = st.button("Run Prediction")
-            if submit:
-                runVideo(model, uploaded_video_path, pred_view, warning)
-
-    elif src == 'From example data.':
-        # Image selector slider
-        videopaths = glob.glob('data/example_videos/*')
-        if len(videopaths) == 0:
-            st.error(
-                'No videos found, Please upload example videos in data/example_videos', icon="⚠️")
-            return
-        imgsel = st.slider('Select random video from example data.',
-                           min_value=1, max_value=len(videopaths), step=1)
-        pred_view = st.empty()
-        video = videopaths[imgsel-1]
-        submit = st.button("Predict!")
-        if submit:
-            runVideo(model, video, pred_view, warning)
+                    fig, ax = plt.subplots(figsize=(img.width / 100, img.height / 100))
+                    ax.imshow(img)
+                    for pred in sorted_predictions:
+                        box = pred['box']
+                        rect = plt.Rectangle((box['x1'], box['y1']), box['x2'] - box['x1'], box['y2'] - box['y1'],
+                                        fill=False, color='blue', linewidth=2)
+                        ax.add_patch(rect)
+                        font_size = int(img.width / 80)  # Adjust font size proportionally
+                        plt.text(box['x1'], box['y1'], pred['name'], color='black', backgroundcolor='white', fontsize=font_size)
+                    plt.axis('off')
+                    st.pyplot(fig, bbox_inches='tight', pad_inches=0)
+                
+                st.subheader("Model Prediction : ")
+                st.metric(label = 'License Plate', value = combined_classes)
 
 
 def main():
@@ -149,16 +200,30 @@ def main():
 
     st.set_page_config(initial_sidebar_state = 'collapsed')
 
-    st.title("Akhirnya Deployment ANJINGGGG")
+    st.title("Big Data Challenge Model Deployment")
 
     tab1, tab2 = st.tabs(["Model Description", "Prediction"])
 
     with tab1:
-        with st.expander("What is YOLO ?") :
+        with st.expander("What We Do") :
+            st.subheader("Object Detection")
+            st.write("""
+            Object detection merupakan teknik dalam pengolahan citra dan penglihatan komputer untuk mengidentifikasi dan melokalisasi objek-objek tertentu dalam gambar atau video. Tujuan utama dari object detection adalah mengenali dan menempatkan kotak pembatas (bounding box) di sekitar objek-objek yang ada dalam gambar. Teknik ini menggabungkan konsep dari deteksi objek (mengenali objek mana yang ada) dan segmentasi semantik (mengenali di mana objek tersebut berada).
+
+            Salah satu pemanfaatan object detection yang umum digunakan adalah pengidentifikasian plat nomor dalam gambar dengan menempatkan kotak pembatas (bounding box) di sekitar area plat nomor. Algoritma object detection untuk plat nomor umumnya menggunakan teknik deep learning, terutama dengan menggunakan model YOLOv5xu yang telah dilatih sebelumnya untuk mengidentifikasi dan mendeteksi plat nomor pada gambar atau video.""")
+
+
+        with st.expander("Model In Use") :
+            st.subheader("YOLOv5xu")
             gambar_yolo = Image.open('assets/yolo.png')
             yolo = gambar_yolo.resize((400,400))
             st.image(yolo)
-            st.write("Penjelasan tentang YOLO blablabla")
+            st.write("""
+            YOLOv5xu adalah varian yang dikembangkan dari model YOLOv5x (You Only Look Once version 5x) yang dioptimalkan khusus untuk tugas deteksi plat nomor pada kendaraan. Konsep dasar YOLOv5xu tetap mengadopsi prinsip "You Only Look Once", di mana model dapat mendeteksi objek dengan cepat dan efisien dalam satu tahap. Dalam konteks deteksi plat nomor, YOLOv5xu mampu memberikan kemampuan yang ditingkatkan dalam mengenali plat nomor pada berbagai situasi dan kondisi visual.
+
+            YOLOv5xu dirancang untuk memberikan efisiensi tinggi dalam melakukan deteksi plat nomor. Dengan pendekatan deteksi dalam satu tahap, model ini dapat mengidentifikasi lokasi dan kelas plat nomor dengan cepat tanpa memerlukan tahap tambahan. Model ini juga mampu melakukan deteksi pada berbagai skala objek. Hal ini sangat berguna dalam deteksi plat nomor pada kendaraan dengan ukuran yang beragam, baik dari jarak dekat maupun jauh. Dengan didukung oleh penggunaan teknologi terkini, YOLOv5xu memiliki tingkat akurasi yang tinggi dalam mengenali plat nomor. Hal ini memungkinkan model untuk mengatasi tantangan seperti pencahayaan yang berbeda, variasi posisi kendaraan, dan perubahan sudut pandang.
+            
+            YOLOv5xu dapat diadaptasi untuk berbagai aplikasi yang memerlukan deteksi plat nomor, seperti sistem pengawasan lalu lintas, pemantauan parkir otomatis, dan keamanan. Dengan demikian, website ini hadir dengan teknik YOLOv5xu untuk deteksi plat nomor kendaraan yang dapat memberikan hasil deteksi yang konsisten dan handal pada berbagai situasi.""")
         
         with st.expander("Model Configuration") :
             st.subheader("Hyperparameters")
@@ -173,6 +238,7 @@ def main():
 
             param_df = pd.DataFrame(data = params, columns = ['Params','Desc.','Value'])
             st.dataframe(data = param_df, hide_index = True, use_container_width = True)
+
     
     with tab2:
          # -- Sidebar
@@ -183,21 +249,27 @@ def main():
         if CFG_ENABLE_VIDEO_PREDICTION:
             option = st.sidebar.radio("Data Input Type : ", ['Image', 'Video'])
         else:
-            option = st.sidebar.radio("Data Input Type : ", ['Image'])
-        if torch.cuda.is_available():
-            deviceoption = st.sidebar.radio("Select compute Device : ", [
-                                        'cpu', 'cuda'], disabled = False, index=1)
-        else:
-            deviceoption = st.sidebar.radio("Select compute Device : ", [
-                                        'cpu', 'cuda'], disabled = True, index=0)
+            option = st.sidebar.radio("Data Input Type : ", ['Image'])  
         # -- End of Sidebar
 
-        st.header('Big Data Challenge Model Deployment')
+        st.header('Model Prediction')
 
-        if option == "Image":
-            imageInput(model = model, src = datasrc)
-        elif option == "Video":
-            videoInput(model = model, src = datasrc)
+        input_type = st.radio('Select Input Number : ', ('Single','Multiple'))
+
+        # Initialize Session State
+        if 'results_df' not in st.session_state:
+            st.session_state.results_df = None
+        if 'image_outputs' not in st.session_state:
+            st.session_state.image_outputs = []
+        if 'output_tab' not in st.session_state:
+            st.session_state.output_tab = False
+        if 'input_type' not in st.session_state:
+            st.session_state.input_type = "Single"
+
+        if option == "Image" and input_type == "Single":
+            imageInput(model = model, src = datasrc, input_type = 'Single')
+        elif option == "Image" and input_type == "Multiple":
+            imageInput(model = model, src = datasrc, input_type = 'Multiple')
 
 
 
